1c199e9094016251b1840ef5d1331447e7da69ff
aydgnme <mertaydogn0@gmail.com>
2026-01-17 21:39:57 +0200

    feat: Implement dashboard with a time wheel displaying tasks and a live clock using new ViewModel and Task model.

---
+++ KREIS/Features/Dashboard/Model/Task.swift
@@ -0,0 +1,12 @@
+//
+//  Task.swift
+//  KREIS
+//
+//  Created by Mert Aydogan on 17.01.2026.
+//
+
+import UIKit
+
+class Task: Identifiable {
+
+}
---
+++ KREIS/Features/Dashboard/ViewModel/DashboardViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  DashboardViewModel.swift
+//  KREIS
+//
+//  Created by Mert Aydogan on 17.01.2026.
+//
+
+import Foundation
