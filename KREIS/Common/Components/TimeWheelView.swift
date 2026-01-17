2ecb3bf5b6e73de7cdaf524831e85735b91c9881
aydgnme <mertaydogn0@gmail.com>
2026-01-17 17:08:00 +0200

    feat: Implement programmatic UI setup by removing storyboard references, refactor color definitions, and introduce TimeWheelView in DashboardViewController.

---
+++ KREIS/Common/Components/TimeWheelView.swift
@@ -0,0 +1,20 @@
+//
+//  TimeWheelView.swift
+//  KREIS
+//
+//  Created by Mert Aydogan on 17.01.2026.
+//
+
+import UIKit
+
+class TimeWheelView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
