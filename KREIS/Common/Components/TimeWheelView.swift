3680157f01d5a6f061748d24529cc0c6e280aa2a
aydgnme <mertaydogn0@gmail.com>
2026-01-17 20:19:10 +0200

    feat: Implement major and minor tick marks for the TimeWheelView to visually represent hours.

--- KREIS/Common/Components/TimeWheelView.swift
+++ KREIS/Common/Components/TimeWheelView.swift
@@ -14,6 +14,11 @@ final class TimeWheelView: UIView {
         static let lineWidth: CGFloat = 24.0
         static let startAngle: CGFloat = -CGFloat.pi / 2
         static let endAngle: CGFloat = 1.5 * CGFloat.pi
+        
+        static let totalHours = 24
+        static let majorTickLength: CGFloat = 20.0
+        static let minorTickLength: CGFloat = 10.0
+        static let tickWidth: CGFloat = 2.0
     }
     
     private var lastFrame: CGRect = .zero
@@ -35,6 +40,11 @@ final class TimeWheelView: UIView {
         layer.lineWidth = Constants.lineWidth
         layer.lineCap = .butt
         layer.strokeEnd = 0.0
+        return layer
+    }()
+    
+    private lazy var ticksLayer: CALayer = {
+        let layer = CALayer()
         return layer
     }()
     
@@ -60,6 +70,7 @@ final class TimeWheelView: UIView {
     
     // MARK: - Setup
     private func setupLayers() {
+        layer.addSublayer(ticksLayer)
         layer.addSublayer(trackLayer)
         layer.addSublayer(progressLayer)
     }
@@ -76,8 +87,41 @@ final class TimeWheelView: UIView {
             clockwise: true
         )
         
-        trackLayer.path = circularPath.cgPath
+        configureTicks()
         progressLayer.path = circularPath.cgPath
+        
+        
+    }
+    
+    // MARK: - Ticks Logic
+    private func configureTicks() {
+        ticksLayer.sublayers?.forEach {
+            $0.removeFromSuperlayer()
+        }
+        
+        let centerPoint = CGPoint(x: bounds.midX, y: bounds.midY)
+        let radius = (min(bounds.width, bounds.height) - Constants.lineWidth) / 2 - 25
+        
+        for i in 0..<Constants.totalHours {
+            let angle = CGFloat(i) * (2.0 * CGFloat.pi / CGFloat(Constants.totalHours)) - (CGFloat.pi / 2)
+            
+            let isMajor = i % 6 == 0
+            let length = isMajor ? Constants.majorTickLength : Constants.minorTickLength
+            
+            let tick = CALayer()
+            tick.backgroundColor = isMajor ? UIColor.kreisBlack.cgColor : UIColor.kreisBlack.withAlphaComponent(0.5).cgColor
+            
+            tick.bounds = CGRect(x: 0, y: 0, width: Constants.tickWidth, height: length)
+            
+            let x = centerPoint.x + radius * cos(angle)
+            let y = centerPoint.y + radius * sin(angle)
+            tick.position = CGPoint(x: x, y: y)
+            
+            tick.transform = CATransform3DMakeRotation(angle + CGFloat.pi / 2, 0, 0, 1)
+            
+            ticksLayer.addSublayer(tick)
+        }
+        
     }
     
     // MARK: - Public Methods
